6c2d48113b64e01dfdce7d92daee7cd7c8e70de0
shaurikd-unc <shaurikd@email.unc.edu>
2022-05-08 16:00:01 -0400

    Initial Commit

---
+++ akariapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  akariapp
+//
+//  Created by Shaurik Deshpande on 5/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Akari")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ akariapp/akariappApp.swift
@@ -0,0 +1,17 @@
+//
+//  akariappApp.swift
+//  akariapp
+//
+//  Created by Shaurik Deshpande on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct akariappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
