8563dff4cfd964cb3f6f1247f9276d9d367ed212
shaurikd-unc <shaurikd@email.unc.edu>
2022-05-12 10:42:55 -0400

    ..

---
+++ akariapp/views/MainMenu.swift
@@ -0,0 +1,97 @@
+//
+//  File.swift
+//  akariapp
+//
+//  Created by Shaurik Deshpande on 5/9/22.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    
+    @State private var switchedOn = true
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [Color("Background")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack  {
+                HStack {
+                    Spacer()
+                    NavigationLink(destination: Text("tbd - settings"), label: {
+                        Image(systemName: "gear")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 50, weight: .medium, design: .default))
+                            .padding()
+                    })
+                }
+                Spacer()
+                
+                BulbLogo(switchedOn: $switchedOn)
+                
+                Spacer()
+                MenuButtons()
+                    .padding()
+            }
+        }
+    }
+}
+
+struct MenuButtons: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            NavigationLink(destination: LevelSelect(), label: {
+                Text("Levels")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+            
+            NavigationLink(destination: Text("tbd - how to play"), label: {
+                Text("How To Play")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+            
+            NavigationLink(destination: Text("tbd - rate us"), label: {
+                Text("Rate Us")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+            
+            NavigationLink(destination: Text("tbd - high scores"), label: {
+                Text("High Scores")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            })
+        }
+    }
+}
+
+struct BulbLogo: View {
+    
+    @Binding var switchedOn: Bool
+    
+    var body: some View {
+        Image("title_logo")
+            .resizable()
+            .scaledToFit()
+        
+        Image(switchedOn ? "lit_bulb" : "bulb_logo")
+            .padding(.horizontal)
+            .onTapGesture {
+                switchedOn.toggle()
+            }
+    }
+}
